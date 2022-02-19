017da45f3fbf5a094d0f638ed508fcbdc9b108d5
VasilevDmitry83 <VasilevDmitry83@mail.ru>
2022-02-19 16:48:27 +0300

    base

---
+++ gb-rest/src/main/java/ru/gb/rest/entity/Cart.java
@@ -0,0 +1,51 @@
+//package ru.gb.rest.entity;
+//
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Setter
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@Entity
+//@Table(name = "cart")
+//public class Cart {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String status = "not empty";
+//
+//    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE})
+//    @JoinTable(name = "cart_product",
+//    joinColumns = @JoinColumn(name = "cart_id"),
+//    inverseJoinColumns = @JoinColumn(name = "product_id"))
+//
+////    private Set<Product> products = new HashSet<>();
+//
+//    private List<Product> products = new ArrayList<>();
+//
+//    public boolean addProduct(Product product) {
+//        return products.add(product);
+//    }
+//
+//    public void deleteProduct(Product product) {
+//        int itemp = -1;
+//        for (int i = 0; i < products.size(); i++) {
+//            if (products.get(i).getId().equals(product.getId())){
+//                itemp = i;
+//                break;
+//            }
+//        }
+//        products.remove(itemp);
+//    }
+//
+//    public List<Product> getProducts(){
+//        return products;
+//    }
+//}
